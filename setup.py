551a96e4a4db0cbac97de8f8ef979123e4f2aecf
Jake Gillespie <jake.gillespie@onefinestay.com>
2016-05-14 14:19:46 +0100

    Prep for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'twitterads',
+    packages = ['twitterads'],
+    version = '0.1',
+    description = 'A wrapper for the Twitter Advertising API',
+    author = 'Jacob Gillespie',
+    author_email = 'jdgillespie91@gmail.com',
+    url = 'https://github.com/jdgillespie91/twitter-ads-api',
+    download_url = 'https://github.com/jdgillespie91/twitter-ads-api/tarball/0.1',
+    keywords = ['twitter', 'advertising', 'api', 'wrapper'],
+    classifiers = []
+)
