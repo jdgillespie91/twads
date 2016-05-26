8c6a52251ce69969e3312dd7e24948f0c3e10caf
Jake Gillespie <jake.gillespie@onefinestay.com>
2016-05-26 23:21:29 +0100

    Fix versioning

--- setup.py
+++ setup.py
@@ -2,19 +2,16 @@ import re
 from distutils.core import setup
 
 
-with open('VERSION', 'r') as f:
-    try:
-        version = re.search('^v([0-9.]*)', f.read()).group(1)
-    except AttributeError:
-        raise RuntimeError('Version not found')
-
-
 setup(
     name='twads',
     packages=['twads'],
-    version=version,
+    version='0.1.2',
     description='A wrapper for the Twitter Ads API',
     author='Jacob Gillespie',
     author_email='jdgillespie91@gmail.com',
-    url='https://github.com/jdgillespie91/twads'
+    url='https://github.com/jdgillespie91/twads',
+    install_requires=[
+        'requests',
+        'requests_oauthlib'
+    ]
 )
