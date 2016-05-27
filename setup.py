ab6e59902c2703a894fa6246678a1f9b5adb7702
Jake Gillespie <jake.gillespie@onefinestay.com>
2016-05-28 00:06:28 +0100

    [auto] Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='twads',
     packages=['twads'],
-    version='0.1.5',
+    version='0.1.6',
     description='A wrapper for the Twitter Ads API',
     author='Jacob Gillespie',
     author_email='jdgillespie91@gmail.com',
