9ca3b6f717e66aa6dd9a8b3dfc8cf71dd2fb304b
Jake Gillespie <jake.gillespie@onefinestay.com>
2016-06-05 11:12:53 +0100

    Fix versioning

--- version.sh
+++ version.sh
@@ -9,35 +9,17 @@ echo "Current version: ${version}"
 release_version=${version/%${version##*.}/$((${version##*.}+1))}
 echo "Release version: ${release_version}"
 
-echo "DEBUG: ${setup_file}"
-echo "DEBUG: $(ls "${setup_file}")"
-
 # Add the version, commit to master and push.
 sed -i "s/${version}/${release_version}/" "${setup_file}"
 
 # Git stuff
 cd ${travis_build_dir}
 
-echo "DEBUG: Setting user"
 git config user.email "jdgillespie91@gmail.com"
 git config user.name "Jake Gillespie"
-echo "DEBUG: $(git config --get user.email)"
-echo "DEBUG: $(git config --get user.name)"
-
-echo "DEBUG: First status"
-git status
-echo "DEBUG: Add"
 git add setup.py
-echo "DEBUG: Second status"
-git status
-echo "DEBUG: Commit"
 git commit -m "[ci skip] Bump version"
-echo "DEBUG: URL"
-echo "DEBUG: $(git remote show origin)"
-echo "DEBUG: Changing URL"
-git remote set-url origin "https://15dee39ff8dc11de98cc7170ef085245c7123154@github.com/jdgillespie91/twads.git"
-echo "DEBUG: ${TWADS_TOKEN}"
-echo "DEBUG: Push"
+git remote set-url origin "https://${TWADS_TOKEN}@github.com/jdgillespie91/twads.git"
 git push origin master
 
 # Create the release.
