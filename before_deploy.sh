22dfa0f63d4f409b3687a1a032a0efbf32df9e33
Jake Gillespie <jake.gillespie@onefinestay.com>
2016-06-05 15:41:52 +0100

    Refactor deployment script

---
+++ before_deploy.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+function setup {
+    # Travis build directory.
+    cd "/home/travis/build/jdgillespie91/twads"
+
+    # Set up git to allow pushing from this repo.
+    git config user.email "jdgillespie91@gmail.com"
+    git config user.name "Jake Gillespie"
+    git remote set-url origin "https://${TWADS_TOKEN}@github.com/jdgillespie91/twads.git"
+}
+
+function bump_version {
+    # Determine release by incrementing patch number of current version.
+    version=$(grep version "setup.py" | cut -d\' -f2)
+    release_version=${version/%${version##*.}/$((${version##*.}+1))}
+
+    echo "current version: ${version}"
+    echo "release version: ${release_version}"
+
+    sed -i "s/${version}/${release_version}/" "setup.py"
+
+    if [ $? -eq 0 ]; then
+        echo "version bumped successfully"
+    fi
+}
+
+function push_version {
+    # We need to prevent the build here since we're pushing to master.
+    git add setup.py
+    git commit -m "[ci skip] Bump version"
+    git branch bump-version
+    git checkout master
+    git merge bump-version
+    git push origin master
+}
+
+function push_tags {
+    git tag -a "${release_version}" -m "Version ${release_version}"
+    git push origin "refs/tags/${release_version}"
+    curl -s -X POST -H "Authorization: Basic amRnaWxsZXNwaWU5MTpjMmM5NTdlODc2MGQxNDlkNDUwM2MxNDBiNWY1ZWRlYWVmMGM5NTc2" -H "Accept: application/vnd.github.v3+json" -H "Content-Type: application/json" -d '{"tag_name": "'${release_version}'"}' "https://api.github.com/repos/jdgillespie91/twads/releases"
+}
+
+function main {
+    setup
+    bump_version
+    push_version
+    push_tags
+}
+
+main
