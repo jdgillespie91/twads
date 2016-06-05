015c5f7d60b5b32acef373d53e0dcefc91bd797f
Jake Gillespie <jake.gillespie@onefinestay.com>
2016-06-05 10:52:24 +0100

    Include token

--- version.sh
+++ version.sh
@@ -35,7 +35,7 @@ git commit -m "[ci skip] Bump version"
 echo "DEBUG: URL"
 echo "DEBUG: $(git remote show origin)"
 echo "DEBUG: Changing URL"
-git remote set-url "https://15dee39ff8dc11de98cc7170ef085245c7123154@github.com/jdgillespie91/twads.git"
+git remote set-url origin "https://15dee39ff8dc11de98cc7170ef085245c7123154@github.com/jdgillespie91/twads.git"
 echo "DEBUG: Push"
 git push origin master
 
